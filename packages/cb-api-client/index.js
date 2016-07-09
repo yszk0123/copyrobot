8ee1f92536af0393cd891959c41fd58b84474de5
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-07-09 22:49:08 +0900

    Use modular file structure

--- packages/cb-api-client/index.js
+++ packages/cb-api-client/index.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-// import request from 'request-promise';
 import fs from 'fs';
 import config from './config';
 const MALE = 'male';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 'use strict';
 var path = require('path');
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
+var autoprefixer = require('autoprefixer');
 
 module.exports = {
   devtool: 'eval-source-map',
@@ -8,18 +11,27 @@ module.exports = {
     __dirname: false
   },
   entry: {
-    'app': './src/app'
+    'app': './packages/cb-app'
   },
   output: {
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, 'public/build'),
     filename: '[name].js',
     publicPath: '/assets/'
   },
   resolve: {
+    root: [
+      path.resolve('./node_modules'),
+      path.resolve('./packages')
+    ],
     extensions: ['', '.webpack.js', '.web.js', '.js', '.jsx', '.json']
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
+    new ExtractTextWebpackPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      title: 'copybot',
+      filename: 'app.html'
+    }),
     new webpack.DefinePlugin({
       '__DEVELOPMENT__': true,
       'process.env': {
@@ -32,13 +44,27 @@ module.exports = {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: 'babel-loader?presets[]=react&presets[]=es2015',
+        loader: 'babel-loader?cacheDirectory',
         include: /node_modules/
       },
       {
-        test: /\.jsoon?$/,
+        test: /\.css$/,
+        loader: ExtractTextWebpackPlugin.extract('style-loader', 'css-loader'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.json?$/,
         loader: 'json-loader'
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'file-loader'
       }
     ]
-  }
+  },
+  postcss: [autoprefixer]
 };
