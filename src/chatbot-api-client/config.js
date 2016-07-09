24913e833f0a9ee5658129160fd4ccd41193aaa2
Yuji Suzuki <y0suzuki0123@gmail.com>
2016-07-09 22:32:20 +0900

    Implement communication logic

---
+++ src/chatbot-api-client/config.js
@@ -0,0 +1,9 @@
+export default {
+  chatbotApi: {
+    endpoint: 'https://chatbot-api.userlocal.jp/api/chat',
+    keys: {
+      female: '1fecf3fd500de1298bb9',
+      male: '4b301654bd07e9a42272'
+    }
+  }
+}
---
+++ src/chatbot-api-client/index.js
@@ -0,0 +1,72 @@
+import axios from 'axios';
+// import request from 'request-promise';
+import fs from 'fs';
+import config from './config';
+const MALE = 'male';
+const FEMALE = 'female';
+
+function getChat(key, message) {
+  const params = {
+    message,
+    key
+  };
+
+  return axios.get(config.chatbotApi.endpoint, { params }).then((response) => {
+    return response.data;
+  });
+}
+
+class Client {
+  constructor() {
+    this._messageLog = [];
+  }
+
+  getKeyOf(person) {
+    return config.chatbotApi.keys[person];
+  }
+
+  log(data) {
+    this._messageLog.push(data);
+  }
+
+  getLog() {
+    return this._messageLog;
+  }
+
+  async communicate(person, message) {
+    const key = this.getKeyOf(person);
+
+    const response = await getChat(key, message);
+
+    const data = {
+      name: person,
+      message: response.result
+    };
+    // console.log('RESPONSE:', data);
+    this.log(data);
+    return data;
+  }
+
+  async communicateAll(count, initialMessage) {
+    let message = initialMessage;
+
+    for (let i = 0; i < count; i += 1) {
+      message = await this.communicate(MALE, message);
+      message = await this.communicate(FEMALE, message);
+    }
+
+    return this.getLog();
+  }
+}
+
+function main() {
+  const client = new Client();
+  client.communicateAll(2, 'hello').then((log) => {
+    fs.writeFileSync('result.log', JSON.stringify(log), 'utf8');
+    console.log(log);
+  }).catch((error) => {
+    console.error(error);
+  });
+}
+
+main();
